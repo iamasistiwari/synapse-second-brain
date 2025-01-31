a3029052c8f59d1fe6dad0c304233e0450dc27ef
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-01-31 23:41:29 +0530

    submit logic added

--- apps/web/app/addcontent/page.tsx
+++ apps/web/app/addcontent/page.tsx
@@ -1,23 +1,76 @@
 'use client';
 import Button from '@/components/Button';
 import EditorComponent from '@/components/Editor';
-import { Link, Notebook, Plus, Send, Twitter } from 'lucide-react';
-import React, { FormEvent, useState } from 'react';
+import TagInput from '@/components/TagInput';
+import { FileText, Link, Send, Twitter } from 'lucide-react';
+import React, { FormEvent, useCallback, useState } from 'react';
+import { ContentType } from '@repo/backend-common/types';
+import { addContentValidator } from '@repo/backend-common/types';
+import toast from 'react-hot-toast';
+import { z } from 'zod';
 
-// type FormData = ContentType;
-
-type ContentType = 'link' | 'tweet' | 'note';
+export type SelectType = 'Link' | 'Tweet' | 'Note';
 
 export default function page() {
-  const [selected, setSelected] = useState<ContentType>('note');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [formData, setFormData] = useState<ContentType>({
+    type: 'Note',
+    title: '',
+    url: '',
+    description: '',
+    tags: [] as string[],
+  });
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSelection = useCallback((type: SelectType) => {
+    setFormData({
+      type: type,
+      title: '',
+      url: '',
+      description: '',
+      tags: [],
+    });
+
+  }, [])
+
+  const handleSubmit = async () => {
+    setLoading(true)
+    try {
+      addContentValidator.parse(formData);
+      if(formData.type === 'Link' || formData.type === 'Tweet'){
+        const titleValidation = z.string().min(5).safeParse(formData.title)
+        if(!titleValidation.success){
+          toast.error("title must be of atleast of 5 char", {duration: 1000})
+          return
+        }
+        const urlValidation = z.string().url().safeParse(formData.url)
+        if(!urlValidation.success){
+          toast.error('url must be valid', { duration: 1000 });
+          return;
+        }
+        // make backend request
+
+      }
+
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        toast.error('invalid data please add atleast one tag', { duration: 1000 });
+        return;
+      }
+      toast.error('Something went wrong', { duration: 1000 });
+      return;
+    } finally{
+      new Promise(() => {
+        setTimeout(() => {
+          setLoading(false)
+        }, 1000)
+      })
+    }
   };
 
   return (
     <div className="mt-11 flex h-screen justify-center">
-      <form onSubmit={handleSubmit}>
+      {/* <div>{JSON.stringify(formData)}</div> */}
+      <div>
         <div className="flex w-full flex-col">
           <div className="flex w-[80vw] items-center justify-center">
             <div className="flex flex-col">
@@ -31,10 +84,10 @@ export default function page() {
                 <div id="type" className="flex gap-x-3">
                   <Button
                     isLoading={false}
-                    Icon={Notebook}
-                    className={`${selected === 'note' ? 'bg-neutral-700 transition-all duration-100' : ''}`}
+                    Icon={FileText}
+                    className={`${formData.type === 'Note' ? 'bg-neutral-700 transition-all duration-100' : ''}`}
                     onClick={() => {
-                      setSelected('note');
+                      handleSelection('Note');
                     }}
                   >
                     Note
@@ -42,9 +95,9 @@ export default function page() {
                   <Button
                     isLoading={false}
                     Icon={Twitter}
-                    className={`${selected === 'tweet' ? 'bg-neutral-700 transition-all duration-100' : ''}`}
+                    className={`${formData.type === 'Tweet' ? 'bg-neutral-700 transition-all duration-100' : ''}`}
                     onClick={() => {
-                      setSelected('tweet');
+                      handleSelection('Tweet');
                     }}
                   >
                     Tweet
@@ -52,20 +105,29 @@ export default function page() {
                   <Button
                     isLoading={false}
                     Icon={Link}
-                    className={`${selected === 'link' ? 'bg-neutral-700 transition-all duration-100' : ''}`}
+                    className={`${formData.type === 'Link' ? 'bg-neutral-700 transition-all duration-100' : ''}`}
                     onClick={() => {
-                      setSelected('link');
+                      handleSelection('Link');
                     }}
                   >
                     Link
                   </Button>
                 </div>
-                <Button className='bg-green-700' isLoading={false} Icon={Send}>Submit</Button>
+                <Button
+                  className="bg-green-700"
+                  isLoading={loading}
+                  Icon={Send}
+                  onClick={() => {
+                    handleSubmit();
+                  }}
+                >
+                  Submit
+                </Button>
               </div>
             </div>
           </div>
 
-          {selected !== 'note' ? (
+          {formData.type !== 'Note' ? (
             <div className="mb-5 flex flex-col">
               <label
                 className="mb-1 pl-1 tracking-wider opacity-80"
@@ -79,11 +141,14 @@ export default function page() {
                 name="title"
                 className="border-custom h-11 w-full rounded-lg border bg-transparent pl-3 text-black focus:outline-none dark:text-white"
                 placeholder="title"
+                onChange={(e) =>
+                  setFormData((c) => ({ ...c, title: e.target.value }))
+                }
               ></input>
             </div>
           ) : null}
 
-          {selected === 'tweet' ? (
+          {formData.type === 'Tweet' ? (
             <div className="mb-5 flex flex-col">
               <label
                 className="mb-1 pl-1 tracking-wider opacity-80"
@@ -96,11 +161,14 @@ export default function page() {
                 id="tweet url"
                 className="border-custom h-11 w-full rounded-lg border bg-transparent pl-3 text-blue-600 focus:outline-none dark:text-blue-500"
                 placeholder="tweet url"
+                onChange={(e) =>
+                  setFormData((c) => ({ ...c, url: e.target.value }))
+                }
               ></input>
             </div>
           ) : null}
 
-          {selected === 'link' ? (
+          {formData.type === 'Link' ? (
             <div className="mb-5 flex flex-col">
               <label
                 className="mb-1 pl-1 tracking-wider opacity-80"
@@ -113,19 +181,33 @@ export default function page() {
                 id="url"
                 className="border-custom h-11 w-full rounded-lg border bg-transparent pl-3 text-blue-600 focus:outline-none dark:text-blue-500"
                 placeholder="url"
+                onChange={(e) =>
+                  setFormData((c) => ({ ...c, url: e.target.value }))
+                }
               ></input>
             </div>
           ) : null}
 
-          {selected === 'note' ? (
+          {formData.type === 'Note' ? (
             <div className="mt-5 flex flex-col">
               <div className="border-custom w-full rounded-lg border">
-                <EditorComponent />
+                <EditorComponent formData={formData} setFormData={setFormData}/>
               </div>
             </div>
           ) : null}
+
+          {/*  */}
+          <div className="mt-5 flex flex-col">
+            <label
+              className="mb-1 pl-1 tracking-wider opacity-80"
+              htmlFor="tweet url"
+            >
+              Enter a tag*
+            </label>
+            <TagInput formData={formData} setFormData={setFormData}/>
+          </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
