f91838fdeeca8a874214053253e339a8e4add7e2
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-02-25 21:11:40 +0530

    deploy-docker-vps

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,6 @@ import { Brain, Link2, Search } from 'lucide-react';
 import LoginDailog from '@/components/LoginDailog';
 import Link from 'next/link';
 export default function Page() {
-  console.log("HERE IS ENV", process.env.NEXTAUTH_URL)
   return (
     <div className="h-screen text-white">
       <div className="absolute inset-0 -z-10 h-screen w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
