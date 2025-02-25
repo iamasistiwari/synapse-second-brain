1a3c49cb456de5a081ec23d7c9ac965e7cac31ad
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-02-25 20:59:50 +0530

    deploy-docker-vps

--- apps/http-backend/src/helpers/checkEnvs.ts
+++ apps/http-backend/src/helpers/checkEnvs.ts
@@ -1,20 +1,21 @@
 import dotenv from "dotenv";
-dotenv.config();
-export default function checkEnv() {
-  const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
-  const SUPABASE_URL = process.env.SUPABASE_URL;
-  const SUPABASE_KEY = process.env.SUPABASE_KEY;
-  const SECRET_KEY = process.env.SECRET_KEY;
-  const NODE_ENV = process.env.NODE_ENV;
 
-  if (
-    !NEXTAUTH_SECRET ||
-    !SUPABASE_URL ||
-    !SUPABASE_KEY ||
-    !SECRET_KEY ||
-    !NODE_ENV
-  ) {
-    throw new Error("ENV VARIABLES IS MISSING");
+dotenv.config();
+
+const requiredEnvVars = [
+  "NEXTAUTH_SECRET",
+  "SUPABASE_URL",
+  "SUPABASE_KEY",
+  "SECRET_KEY",
+  "NODE_ENV",
+] as const;
+
+export default function checkEnv() {
+  const missingVars = requiredEnvVars.filter((key) => !process.env[key]);
+
+  if (missingVars.length > 0) {
+    throw new Error(`Missing ENV variables: ${missingVars.join(", ")}`);
   }
 }
+
 checkEnv();
